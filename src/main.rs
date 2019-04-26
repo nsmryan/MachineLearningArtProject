5deb5437b634abe0aa70a9c3553a5c967953534d
Noah Ryan <noah.m.ryan@nasa.gov>
2019-04-26 17:55:24 -0400

    Generate images

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate fann;
 extern crate libc;
 extern crate imageproc;
+extern crate rand;
 
 use std::boxed::Box;
 use libc::c_uint;
@@ -10,14 +11,33 @@ use image::Rgb;
 
 use imageproc::drawing::*;
 
-use fann::{ActivationFunc, Fann, TrainAlgorithm, QuickpropParams};
+use rand::prelude::*;
+
+use fann::{ActivationFunc, Fann, TrainAlgorithm};
 use fann::TrainData;
 
 
-const WIDTH: i32 = 512;
-const HEIGHT: i32 = 512;
+const WIDTH: u32 = 512;
+const HEIGHT: u32 = 512;
+
 
 fn main() {
+    let mut images = Vec::new();
+
+    let mut rng = rand::thread_rng();
+
+    for index in 0..10 {
+        let mut image = ImageBuffer::<Rgb<u8>, Vec<u8>>::new(WIDTH, HEIGHT);
+
+        let x: i32 = (rng.gen::<f32>() * WIDTH as f32) as i32;
+        let y: i32 = (rng.gen::<f32>() * HEIGHT as f32) as i32;
+        draw_filled_ellipse_mut(&mut image, (x, y), 25, 25, Rgb { data: [128, 50, 0] });
+
+        image.save(format!("./image_{}.png", index)).unwrap();
+
+        images.push(image);
+    }
+
     let mut fann = Fann::new(&[2, 3, 1]).unwrap();
 
     fann.set_activation_func_hidden(ActivationFunc::SigmoidSymmetric);
@@ -40,11 +60,6 @@ fn main() {
     println!("{:?}", fann.run(&[1.0, 0.0]).unwrap());
     println!("{:?}", fann.run(&[1.0, 0.0]).unwrap());
 
-    let mut image = ImageBuffer::<Rgb<u8>, Vec<u8>>::new(WIDTH, HEIGHT);
-
-    draw_filled_ellipse_mut(&mut image, (100, 100), 25, 25, Rgb { data: [128, 50, 0] });
-
-    image.save("./image.png");
 }
 
 fn training_data_callback(index: c_uint) -> (Vec<f32>, Vec<f32>) {
