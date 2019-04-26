ba4da4841a2371c1a3d889765b22b9588e82a84c
Noah Ryan <noah.m.ryan@nasa.gov>
2019-04-26 18:16:36 -0400

    Network runs on generated images

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate imageproc;
 extern crate rand;
 
 use std::boxed::Box;
-use libc::c_uint;
+//use libc::c_uint;
 
 use image::ImageBuffer;
 use image::Rgb;
@@ -19,54 +19,59 @@ use fann::TrainData;
 
 const WIDTH: u32 = 512;
 const HEIGHT: u32 = 512;
+const NUM_EXAMPLES: u32 = 10;
 
+type Image = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 fn main() {
-    let mut images = Vec::new();
+    let mut images: Vec<Vec<u8>> = Vec::new();
 
     let mut rng = rand::thread_rng();
 
-    for index in 0..10 {
-        let mut image = ImageBuffer::<Rgb<u8>, Vec<u8>>::new(WIDTH, HEIGHT);
+    for index in 0..NUM_EXAMPLES {
+        let mut image = Image::new(WIDTH, HEIGHT);
 
         let x: i32 = (rng.gen::<f32>() * WIDTH as f32) as i32;
         let y: i32 = (rng.gen::<f32>() * HEIGHT as f32) as i32;
-        draw_filled_ellipse_mut(&mut image, (x, y), 25, 25, Rgb { data: [128, 50, 0] });
+        draw_filled_ellipse_mut(&mut image, (x, y), 25, 25, Rgb { data: [128, 0, 0] });
 
         image.save(format!("./image_{}.png", index)).unwrap();
 
-        images.push(image);
+        images.push(image.into_vec());
     }
 
-    let mut fann = Fann::new(&[2, 3, 1]).unwrap();
+    let mut fann = Fann::new(&[WIDTH*HEIGHT, 2, WIDTH*HEIGHT]).unwrap();
 
     fann.set_activation_func_hidden(ActivationFunc::SigmoidSymmetric);
     fann.set_activation_func_output(ActivationFunc::SigmoidSymmetric);
 
     fann.set_train_algorithm(TrainAlgorithm::Quickprop(Default::default()));
 
+
     let max_epochs = 50000;
     let epochs_between_reports = 1000;
     let desired_error = 0.001;
 
-    let training_data = TrainData::from_callback(4, 2, 1, Box::new(training_data_callback)).unwrap();
+    let training_data_callback = move |index| {
+        let vec: &Vec<u8> = &images[index as usize];
+        let bytes = image_to_vec(vec);
+        (bytes.clone(), bytes.clone())
+    };
+
+    let training_data = TrainData::from_callback(NUM_EXAMPLES,
+                                                 WIDTH*HEIGHT,
+                                                 WIDTH*HEIGHT,
+                                                 Box::new(training_data_callback)).unwrap();
 
     fann.on_data(&training_data)
         .with_reports(epochs_between_reports)
         .train(max_epochs, desired_error).unwrap();
-
-    println!("{:?}", fann.run(&[1.0, 1.0]).unwrap());
-    println!("{:?}", fann.run(&[0.0, 1.0]).unwrap());
-    println!("{:?}", fann.run(&[1.0, 0.0]).unwrap());
-    println!("{:?}", fann.run(&[1.0, 0.0]).unwrap());
-
 }
 
-fn training_data_callback(index: c_uint) -> (Vec<f32>, Vec<f32>) {
-    let examples = vec!((vec!(1.0, 1.0), vec!(1.0)),
-                        (vec!(1.0, 0.0), vec!(1.0)),
-                        (vec!(0.0, 1.0), vec!(1.0)),
-                        (vec!(0.0, 0.0), vec!(0.0)));
-
-    examples[index as usize].clone()
+fn image_to_vec(image: &Vec<u8>) -> Vec<f32> {
+    image.iter().enumerate() 
+                .filter(|(index, val)| index % 3 == 0)
+                .map(|(index, val)| (*val as f32) / 256.0)
+                .collect::<Vec<f32>>()
 }
+
